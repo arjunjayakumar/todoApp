b205b36437084537bdc406d6a41ccfe6ac577d71
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-09-14 17:44:14 +0530

    feat : Adjust components selectors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './layout/card/card.component';
 
-
-
 @NgModule({
-  declarations: [
-    CardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CardComponent],
+  imports: [CommonModule],
+  exports: [CardComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
