795f60e9c03fac607b980f8644fc1a9dc3c4cb2e
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-09-15 19:17:57 +0530

    feat : Add date picker

--- src/app/features/todo/todo.module.ts
+++ src/app/features/todo/todo.module.ts
@@ -4,9 +4,16 @@ import { TodoRoutingModule } from './todo-routing.module';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [TodoFormComponent],
-  imports: [CommonModule, TodoRoutingModule, CoreModule, SharedModule],
+  imports: [
+    CommonModule,
+    TodoRoutingModule,
+    CoreModule,
+    SharedModule,
+    NgbDatepickerModule,
+  ],
 })
 export class TodoModule {}
