43ca6f4e669fbf342fb9425f77caad54db27a2c1
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-09-16 09:59:58 +0530

    feat : Add task list and list item components

--- src/app/features/todo/todo-routing.module.ts
+++ src/app/features/todo/todo-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { HomePageComponent } from 'src/app/core/layout/home-page/home-page.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
     component: HomePageComponent,
     children: [
       { path: 'form', component: TodoFormComponent },
+      { path: 'list', component: TodoListComponent },
       { path: '', redirectTo: 'form', pathMatch: 'full' },
       { path: '**', redirectTo: 'form', pathMatch: 'full' },
     ],
--- src/app/features/todo/todo.module.ts
+++ src/app/features/todo/todo.module.ts
@@ -5,9 +5,11 @@ import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
-  declarations: [TodoFormComponent],
+  declarations: [TodoFormComponent, TodoListComponent, ListItemComponent],
   imports: [
     CommonModule,
     TodoRoutingModule,
